652d85527be52e304be7aadaff53c23b23e1f4c9
Kaiky <kaikysantosdasilva38@gmail.com>
2024-10-13 12:33:44 -0300

    conserto

--- js/cfw.js
+++ js/cfw.js
@@ -1,4 +1,4 @@
-const url = '/pdf/cfw.pdf';
+const url = 'pdf/cfw.pdf';
 const pdfjsLib = window['pdfjs-dist/build/pdf'];
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.worker.min.js';
